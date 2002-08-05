f7c065304156b3fdf30a8db96c1bafc5ded3d904
H. Peter Anvin <hpa@zytor.com>
2002-08-05 21:41:38 +0000

    Clean up and fix execl(), add <stdarg.h> header which creates va_copy(), add <alloca.h> which invokes the gcc builtin.

--- klibc/execl.c
+++ klibc/execl.c
@@ -3,36 +3,45 @@
  */
 
 #include <stdarg.h>
+#include <alloca.h>
 #include <unistd.h>
 
 int execl(const char *path, const char *arg0, ...)
 {
-  va_list ap;
-  int argc = 0;
-  int size = 256;
-  const char **argv;
+  va_list ap, cap;
+  int argc = 1, rv;
+  const char **argv, **argp;
   const char *arg;
 
-  argv = malloc(size*sizeof(const char *));
-  
-  if ( !argv )
-    return -1;
-
   va_start(ap, arg0);
-  argv[argc++] = arg0;
+  va_copy(cap, ap);
 
+  /* Count the number of arguments */
   do {
-    argv[argc++] = arg = va_arg(ap, const char *);
-    if ( argc >= size ) {
-      argv = realloc(argv, (size <<= 1)*sizeof(const char *));
-      if ( !argv )
-	return -1;
-    }
+    arg = va_arg(cap, const char *);
+    argc++;
   } while ( arg );
 
-  va_end(args);
+  va_end(cap);
+  
+  /* Allocate memory for the pointer array */ 
+  argp = argv = alloca(argc*sizeof(const char *));
+  if ( !argv ) {
+    va_end(ap);
+    return -1;
+  }
 
-  return execve(path, argv, environ);
+  /* Copy the list into an array */
+  *argp++ = arg0;
+  do {
+    *argp++ = arg = va_arg(ap, const char *);
+  } while ( arg );
+
+  rv = execve(path, argv, environ);
+
+  va_end(ap);
+
+  return rv;
 }
 
 
--- klibc/include/unistd.h
+++ klibc/include/unistd.h
@@ -9,10 +9,18 @@
 #include <stddef.h>
 #include <sys/types.h>
 
+__extern char **environ;
 __extern __noreturn _exit(int);
 
-pid_t getpid(void);
-pid_t getppid(void);
+__extern pid_t getpid(void);
+__extern pid_t getppid(void);
+
+__extern pid_t fork(void);
+__extern pid_t vfork(void);
+
+__extern int execve(const char *, const char * const *, char * const *);
+__extern int execl(const char *, const char *, ...);
+__extern int execv(const char *, const char * const *);
 
 __extern ssize_t read(int, void *, size_t);
 __extern ssize_t write(int, const void *, size_t);
