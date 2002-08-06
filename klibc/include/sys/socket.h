441b6e128d318837913191f8147ac6f3e0ef5727
H. Peter Anvin <hpa@zytor.com>
2002-08-06 01:50:51 +0000

    Add headers for all the socket system calls.

--- klibc/include/sys/socket.h
+++ klibc/include/sys/socket.h
@@ -11,6 +11,21 @@
 typedef int socklen_t;
 
 __extern int socket(int, int, int);
+__extern int bind(int, struct sockaddr *, int);
+__extern int connect(int, struct sockaddr *, socklen_t);
+__extern int listen(int, int);
 __extern int accept(int, struct sockaddr *, socklen_t *);
+__extern int getsockname(int, struct sockaddr *, socklen_t *);
+__extern int getpeername(int, struct sockaddr *, socklen_t *);
+__extern int socketpair(int, int, int, int *);
+__extern int send(int, const void *, size_t, unsigned int);
+__extern int sendto(int, const void *, size_t, int, const struct sockaddr *, socklen_t);
+__extern int recv(int, void *, size_t, unsigned int);
+__extern int recvfrom(int, void *, size_t, unsigned int, struct sockaddr *, socklen_t *);
+__extern int shutdown(int, int);
+__extern int setsockopt(int, int, int, const void *, socklen_t);
+__extern int getsockopt(int, int, int, void *, socklen_t *);
+__extern int sendmsg(int, const struct msghdr *, unsigned int);
+__extern int recvmsg(int, struct msghdr *, unsigned int);
 
 #endif /* _SYS_SOCKET_H */
