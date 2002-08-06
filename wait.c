07523435cccbbe1291b9668aeb46a8807d153e6f
H. Peter Anvin <hpa@zytor.com>
2002-08-06 19:37:42 +0000

    Tweak optimizations somewhat.  Implement wait() in terms of wait4().

--- wait.c
+++ wait.c
@@ -2,10 +2,11 @@
  * wait.c
  */
 
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 
 pid_t wait(int *status)
 {
-  return waitpid((pid_t)-1, status, 0);
+  return wait4((pid_t)-1, status, 0, NULL);
 }
