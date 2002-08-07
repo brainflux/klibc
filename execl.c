6d20d4b184458a7a47a0b7e7c2d89b404abc7833
H. Peter Anvin <hpa@zytor.com>
2002-08-07 21:18:05 +0000

    Add support for execle() and lay the groundwork for execlp()

--- execl.c
+++ execl.c
@@ -2,46 +2,7 @@
  * execl.c
  */
 
-#include <stdarg.h>
-#include <alloca.h>
-#include <unistd.h>
-
-int execl(const char *path, const char *arg0, ...)
-{
-  va_list ap, cap;
-  int argc = 1, rv;
-  const char **argv, **argp;
-  const char *arg;
-
-  va_start(ap, arg0);
-  va_copy(cap, ap);
-
-  /* Count the number of arguments */
-  do {
-    arg = va_arg(cap, const char *);
-    argc++;
-  } while ( arg );
-
-  va_end(cap);
-  
-  /* Allocate memory for the pointer array */ 
-  argp = argv = alloca(argc*sizeof(const char *));
-  if ( !argv ) {
-    va_end(ap);
-    return -1;
-  }
-
-  /* Copy the list into an array */
-  *argp++ = arg0;
-  do {
-    *argp++ = arg = va_arg(ap, const char *);
-  } while ( arg );
-
-  rv = execve(path, argv, environ);
-
-  va_end(ap);
-
-  return rv;
-}
-
-
+#define NAME execl
+#define EXEC_P 0
+#define EXEC_E 0
+#include "exec_l.c"
