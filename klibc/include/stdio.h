3a957ffcb0c2a0fe39fac7770606fba85597e9a9
H. Peter Anvin <hpa@zytor.com>
2002-08-09 23:36:40 +0000

    Add sscan(); add sscanf() and vsscanf() to <stdio.h>

--- klibc/include/stdio.h
+++ klibc/include/stdio.h
@@ -29,4 +29,7 @@ __extern int vsprintf(char *, const char *, va_list);
 __extern int snprintf(char *, size_t n, const char *, ...);
 __extern int vsnprintf(char *, size_t n, const char *, va_list);
 
+__extern int sscanf(const char *, const char *, ...);
+__extern int vsscanf(const char *, const char *, va_list);
+
 #endif /* _STDIO_H */
