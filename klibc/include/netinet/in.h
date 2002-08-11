13c7a3fd2452e1806c99105e180385a5fc543244
H. Peter Anvin <hpa@zytor.com>
2002-08-11 04:50:38 +0000

    Add <netinet/in.h> for hton/ntoh macros

---
+++ klibc/include/netinet/in.h
@@ -0,0 +1,45 @@
+/*
+ * netinet/in.h
+ */
+
+#ifndef _NETINET_IN_H
+#define _NETINET_IN_H
+
+#include <stdint.h>
+#include <endian.h>
+
+#ifdef __BIG_ENDIAN
+
+#define htonl(x) ((uint32_t)(x))
+#define ntohl(x) ((uint32_t)(x))
+#define htons(x) ((uint16_t)(x))
+#define ntohs(x) ((uint16_t)(x))
+
+#elif defined(__LITTLE_ENDIAN)
+
+static __inline__
+uint32_t htonl(uint32_t __x)
+{
+#if defined(__i486__)||defined(__pentium__)||defined(__pentiumpro__)
+  asm("bswap %0" : "+rm" (__x));
+  return __x;
+#else
+  return (__x >> 24) + ((__x >> 8) & 0xff00) +
+    ((__x & 0xff00) << 8) + (__x << 24);
+#endif
+}
+
+static __inline__
+uint16_t htons(uint16_t __x)
+{
+  return (__x >> 8) + (__x << 8);
+}
+
+#define htonl(x) htonl(x)
+#define ntohl(x) htohl(x)
+#define htons(x) htons(x)
+#define ntohs(x) htons(x)
+
+#endif /* endian */
+
+#endif /* _NETINET_IN_H */
