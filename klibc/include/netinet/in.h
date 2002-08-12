d580c2d25ede73d4a8568e5d3672080082d52246
H. Peter Anvin <hpa@zytor.com>
2002-08-12 01:30:55 +0000

    The kernels endian handing is better than ours -- use the kernel.

--- klibc/include/netinet/in.h
+++ klibc/include/netinet/in.h
@@ -8,38 +8,9 @@
 #include <stdint.h>
 #include <endian.h>
 
-#if __BYTE_ORDER == __BIG_ENDIAN
-
-#define htonl(x) ((uint32_t)(x))
-#define ntohl(x) ((uint32_t)(x))
-#define htons(x) ((uint16_t)(x))
-#define ntohs(x) ((uint16_t)(x))
-
-#elif __BYTE_ORDER == __LITTLE_ENDIAN
-
-static __inline__
-uint32_t htonl(uint32_t __x)
-{
-#if defined(__i486__)||defined(__pentium__)||defined(__pentiumpro__)
-  asm("bswap %0" : "+rm" (__x));
-  return __x;
-#else
-  return (__x >> 24) + ((__x >> 8) & 0xff00) +
-    ((__x & 0xff00) << 8) + (__x << 24);
-#endif
-}
-
-static __inline__
-uint16_t htons(uint16_t __x)
-{
-  return (__x >> 8) + (__x << 8);
-}
-
-#define htonl(x) htonl(x)
-#define ntohl(x) htohl(x)
-#define htons(x) htons(x)
-#define ntohs(x) htons(x)
-
-#endif /* endian */
+#define htons(x)	__cpu_to_be16(x)
+#define ntohs(x)	__be16_to_cpu(x)
+#define htonl(x)	__cpu_to_be32(x)
+#define ntohl(x)	__be32_to_cpu(x)
 
 #endif /* _NETINET_IN_H */
