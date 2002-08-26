2bb6987a3046c7c5170725adba3c2cf7f15b02f8
H. Peter Anvin <hpa@zytor.com>
2002-08-26 20:24:25 +0000

    More function attribute support; prepare for regparm support (i386)

--- include/klibc/compiler.h
+++ include/klibc/compiler.h
@@ -7,6 +7,7 @@
 #ifndef _KLIBC_COMPILER_H
 #define _KLIBC_COMPILER_H
 
+/* How to declare a function that *must* be inlined */
 #ifdef __GNUC__
 # if __GNUC_MAJOR__ >= 3
 #  define __must_inline static __inline__ __attribute__((always_inline))
@@ -17,10 +18,26 @@
 # define __must_inline inline	/* Just hope this works... */
 #endif
 
+/* How to declare a function that does not return */
 #ifdef __GNUC__
 # define __noreturn void __attribute__((noreturn))
 #else
 # define __noreturn void
+#endif
+
+/* How to declare a "constant" function (a function in the
+   mathematical sense) */
+#ifdef __GNUC__
+# define __constfunc __attribute__((const))
+#else
+# define __constfunc
+#endif
+
+/* Format attribute */
+#ifdef __GNUC__
+# define __formatfunc(t,f,a) __attribute__((format(t,f,a)))
+#else
+# define __formatfunc(t,f,a)
 #endif
 
 #endif
