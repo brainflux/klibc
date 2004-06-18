7c484677f4acdf4ac88c24cc7fbea833d5167aa6
H. Peter Anvin <hpa@zytor.com>
2004-06-18 00:08:03 +0000

    Default to giving real error messages.

--- klibc/strerror.c
+++ klibc/strerror.c
@@ -7,10 +7,17 @@
 char *strerror(int errnum)
 {
   static char message[32] = "error "; /* enough for error 2^63-1 */
-
   char numbuf[32];
   char *p;
   unsigned int e = (unsigned int)errnum;
+
+#ifdef WITH_ERRLIST
+  extern const int sys_nerr;
+  extern const char * const sys_errlist[];
+
+  if ( e < (unsigned int)sys_nerr && sys_errlist[e] )
+    return (char *)sys_errlist[e];
+#endif
 
   p = numbuf+sizeof numbuf;
   *--p = '\0';
