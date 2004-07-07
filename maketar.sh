6584e69392b19f98309af81d550226e9d01d3701
H. Peter Anvin <hpa@zytor.com>
2004-07-07 00:08:45 +0000

    Updates to make rpmbuild -ta work better

---
+++ maketar.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+#
+# Make a tarball from the current CVS tree
+#
+
+if [ -z "$CVS" ]; then
+    CVS=cvs
+fi
+export CVSROOT=`cat CVS/Root`
+
+tmp=/var/tmp/klibc.$$
+rm -rf $tmp
+mkdir -p $tmp
+
+cd $tmp
+
+$CVS export -D now klibc
+rm -f klibc/maketar.sh
+( cd klibc && make klibc.spec )
+version=`cat klibc/version`
+mv klibc klibc-$version
+rm -f ../klibc-$version.tar
+tar cvvf ../klibc-$version.tar klibc-$version
+gzip -9 ../klibc-$version.tar
+cd ..
+rm -rf $tmp
+
