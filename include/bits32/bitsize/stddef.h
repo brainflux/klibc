14b144118dc3bf01f4cd073af5002382d9b5db13
H. Peter Anvin <hpa@zytor.com>
2004-07-07 00:37:02 +0000

    Clean up unnecessary parens

--- include/bits32/bitsize/stddef.h
+++ include/bits32/bitsize/stddef.h
@@ -6,7 +6,7 @@
 #define _BITSIZE_STDDEF_H
 
 #define _SIZE_T
-#if (defined(__s390__) || defined(__hppa__) || defined(__cris__))
+#if defined(__s390__) || defined(__hppa__) || defined(__cris__)
 typedef unsigned long size_t;
 #else
 typedef unsigned int size_t;
