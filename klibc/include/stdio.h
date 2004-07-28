90eb899958a506c4a0980eaaee65be3548706710
H. Peter Anvin <hpa@zytor.com>
2004-07-28 21:09:59 +0000

    gcc needs putchar().  Sigh.

--- klibc/include/stdio.h
+++ klibc/include/stdio.h
@@ -105,6 +105,14 @@ __extern int vsprintf(char *, const char *, va_list);
 __extern int snprintf(char *, size_t n, const char *, ...);
 __extern int vsnprintf(char *, size_t n, const char *, va_list);
 
+/* No buffering, so no flushing needed */
+extern __inline__ int
+fflush(FILE *__f)
+{
+  (void)__f;
+  return 0;
+}
+
 __extern int sscanf(const char *, const char *, ...);
 __extern int vsscanf(const char *, const char *, va_list);
 
