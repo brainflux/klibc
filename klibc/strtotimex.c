e2880125cdeeddf81b869cb80779916ef4a5a49a
H. Peter Anvin <hpa@zytor.com>
2004-08-03 23:49:25 +0000

    Even more efficient implementation to strtotime*()

--- klibc/strtotimex.c
+++ klibc/strtotimex.c
@@ -14,17 +14,17 @@
 char * NAME (const char *str, TIMEX *ts)
 {
   int n;
-  char *s;
+  char *s, *s0;
   __typeof__(ts->FSEC) fs;	/* Fractional seconds */
 
   ts->tv_sec = strtoul(str, &s, 10);
   fs = 0;
 
   if ( *s == '.' ) {
-    s++;
+    s0 = s+1;
 
-    for ( n = 0 ; n < DECIMALS && isdigit(*s) ; n++ )
-      fs = fs*10 + (*s++ - '0');
+    fs = strtoul(s0, &s, 10);
+    n = s-s0;
     
     while ( isdigit(*s) )
       s++;
