f65724cb4a1709b0a78c8da4d3d09f1fc6e77412
H. Peter Anvin <hpa@zytor.com>
2004-08-03 23:54:53 +0000

    Actually correct implementation of strtotime*()

--- klibc/strtotimex.c
+++ klibc/strtotimex.c
@@ -7,23 +7,24 @@
  *
  */
 
-#include <stdlib.h>
 #include <ctype.h>
+#include <inttypes.h>
+#include <stdlib.h>
 #include <sys/time.h>
 
 char * NAME (const char *str, TIMEX *ts)
 {
   int n;
   char *s, *s0;
-  unsigned long fs;		/* Fractional seconds */
+  __typeof__(ts->FSEC) fs;	/* Fractional seconds */
 
-  ts->tv_sec = strtoul(str, &s, 10);
+  ts->tv_sec = strntoumax(str, &s, 10, ~(size_t)0);
   fs = 0;
 
   if ( *s == '.' ) {
     s0 = s+1;
 
-    fs = strtoul(s0, &s, 10);
+    fs = strntoumax(s0, &s, 10, DECIMALS);
     n = s-s0;
     
     while ( isdigit(*s) )
