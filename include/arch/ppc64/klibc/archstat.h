647946f20ceba8e6bf874712c57881e2bae3deac
Olaf Hering <olh@suse.de>
2004-09-19 13:48:35 +0000

    include/arch/ppc64/klibc/archstat.h: struct stat: match reality

--- include/arch/ppc64/klibc/archstat.h
+++ include/arch/ppc64/klibc/archstat.h
@@ -3,26 +3,23 @@
 
 #define _STATBUF_ST_NSEC
 
-/* This matches struct stat64 in glibc2.1. Only used for 32 bit. */
-
 struct stat {
-	unsigned long st_dev; 		/* Device.  */
-	unsigned long st_ino;		/* File serial number.  */
-	unsigned int st_mode;		/* File mode.  */
-	unsigned int st_nlink;		/* Link count.  */
-	unsigned int st_uid;		/* User ID of the file's owner.  */
-	unsigned int st_gid;		/* Group ID of the file's group. */
-	unsigned long st_rdev; 		/* Device number, if device.  */
-	unsigned short __pad2;
-	long st_size;			/* Size of file, in bytes.  */
-	int  st_blksize;		/* Optimal block size for I/O.  */
-
-	long st_blocks;			/* Number 512-byte blocks allocated. */
+	unsigned long	st_dev;
+	ino_t		st_ino;
+	nlink_t		st_nlink;
+	mode_t		st_mode;
+	uid_t 		st_uid;
+	gid_t 		st_gid;
+	unsigned long	st_rdev;
+	off_t		st_size;
+	unsigned long  	st_blksize;
+	unsigned long  	st_blocks;
 	struct timespec st_atim;	/* Time of last access.  */
 	struct timespec st_mtim;	/* Time of last modification.  */
 	struct timespec st_ctim;	/* Time of last status change.  */
-	unsigned int   __unused4;
-	unsigned int   __unused5;
+	unsigned long  	__unused4;
+	unsigned long  	__unused5;
+	unsigned long  	__unused6;
 };
 
 #endif
