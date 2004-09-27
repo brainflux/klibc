f8d198c94c48a255fd59c13b97196de9ffc88fbb
H. Peter Anvin <hpa@zytor.com>
2004-09-27 21:22:57 +0000

    Add prototypes for clone(2)

--- include/sched.h
+++ include/sched.h
@@ -20,4 +20,15 @@ struct sched_param {
 __extern int sched_setscheduler(pid_t, int, const struct sched_param *);
 __extern int sched_yield(void);
 
+/* Raw interfaces to clone(2); only actually usable for non-VM-cloning */
+#ifdef __ia64__
+__extern pid_t __clone2(int, void *, void *);
+static __inline__ pid_t __clone(int _f, void *_sp)
+{
+  return __clone2(_f, _sp, 0);
+}  
+#else
+__extern pid_t __clone(int, void *);
+#endif
+
 #endif /* _SCHED_H */
