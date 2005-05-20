1db8f1a3e7d4f4c2a4b3575471cb4df3318a3276
H. Peter Anvin <hpa@smyrno.hos.anvin.org>
2005-05-20 11:55:18 -0700

    Don't copy the sigaction structure if we don't need to...

--- klibc/sigaction.c
+++ klibc/sigaction.c
@@ -11,10 +11,10 @@ __extern int __rt_sigaction(int, const struct sigaction *, struct sigaction *, s
 
 int sigaction(int sig, const struct sigaction *act, struct sigaction *oact)
 {
-  struct sigaction sa = *act;
-
-  /* x86-64, and the Fedora i386 kernel, are broken without SA_RESTORER */
 #if defined(__i386__) || defined(__x86_64__)
+  /* x86-64, and the Fedora i386 kernel, are broken without SA_RESTORER */
+  struct sigaction sa = *act;
+  act = &sa;
 
   if ( !(sa.sa_flags & SA_RESTORER) ) {
     /* The kernel can't be trusted to have a valid default restorer */
@@ -24,8 +24,8 @@ int sigaction(int sig, const struct sigaction *act, struct sigaction *oact)
 #endif
 
 #ifdef __NR_sigaction
-  return __sigaction(sig, &sa, oact);
+  return __sigaction(sig, act, oact);
 #else
-  return __rt_sigaction(sig, &sa, oact, sizeof(sigset_t));
+  return __rt_sigaction(sig, act, oact, sizeof(sigset_t));
 #endif
 }
