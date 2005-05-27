0124ca9c6ebbc6ce2f6b96246a39dadc2e36a06a
H. Peter Anvin <hpa@smyrno.hos.anvin.org>
2005-05-26 21:30:01 -0700

    Switch from CVS to git

--- maketar.sh
+++ maketar.sh
@@ -1,20 +1,11 @@
 #!/bin/bash -x
 #
-# Make a tarball from the current CVS tree
+# Make a tarball from the current git repository
 #
-
-if [ -z "$CVS" ]; then
-    CVS=cvs
-fi
-export CVSROOT=`cat CVS/Root`
 
 tmp=/var/tmp/klibc.$$
 rm -rf $tmp
-mkdir -p $tmp
-
-cd $tmp
-
-$CVS export -D now klibc
+cg-export $tmp
 rm -f klibc/maketar.sh
 ( cd klibc && make klibc.spec )
 version=`cat klibc/version`
