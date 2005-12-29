36f97418048a4bdaa8053d07aa8e077b179e78ce
H. Peter Anvin <hpa@zytor.com>
2005-12-28 22:30:37 -0800

    <endian.h> is a fairly dangerous header; include only what we need from it.

--- include/byteswap.h
+++ include/byteswap.h
@@ -5,7 +5,8 @@
 #ifndef _BYTESWAP_H
 #define _BYTESWAP_H
 
-#include <endian.h>
+#include <klibc/compiler.h>
+#include <asm/byteorder.h>
 
 #define bswap_16(x) __swab16(x)
 #define bswap_32(x) __swab32(x)
