617221f3534274f00fdb7bb8aa252ba4e5b8400a
H. Peter Anvin <hpa@zytor.com>
2006-01-30 15:59:17 -0800

    Code to load ramdisk, and decompress it if necessary

--- usr/kinit/do_mounts.c
+++ usr/kinit/do_mounts.c
@@ -12,6 +12,8 @@
 
 #include "do_mounts.h"
 #include "kinit.h"
+#include "fstype.h"
+#include "zlib.h"
 
 #define BUF_SZ		65536
 
@@ -365,27 +367,67 @@ ramdisk_load(int argc, char *argv[], dev_t root_dev)
 		arg_ramdisk_start
 		? strtoumax(arg_ramdisk_start, NULL, 10)*ramdisk_blocksize
 		: 0;
-	int rfd, wfd;
+	int rfd;
+	FILE *wfd;
 	char buf[BUF_SZ];
-	ssize_t rv;
+	const char *fstype;
+	unsigned long long fssize;
+	int is_gzip;
+	ssize_t bytes = 0;
 
-	return 0;		/* Not yet implemented */
+	if (prompt_ramdisk) {
+		fprintf(stderr, "Please insert disk for ramdisk and press Enter...\n");
+		getc(stdin);
+	}
 
 	/* XXX: This should be better error checked. */
 	
 	mknod("/dev/rddev", S_IFBLK|0400, root_dev);
 	mknod("/dev/ram0", S_IFBLK|0600, Root_RAM0);
 	rfd = open("/dev/rddev", O_RDONLY);
-	wfd = open("/dev/ram0", O_WRONLY);
+	wfd = fopen("/dev/ram0", "w");
+	
+	/* Check filesystem type */
+	if (identify_fs(rfd, &fstype, &fssize, ramdisk_start) ||
+	    (fssize == 0 && !(is_gzip = !strcmp(fstype, "gzip")))) {
+		fprintf(stderr, "Failure loading ramdisk: unknown filesystem type\n");
+		return 0;
+	}
+
 	lseek(rfd, ramdisk_start, SEEK_SET);
-	
-	/* XXX: Check length of ramdisk; if compressed, we need zlib */
 
-	printf("Loading ramdisk...");
+	fprintf(stderr, "Loading ramdisk...");
+	if (is_gzip) {
+		gzFile gzf = gzdopen(rfd, "r");
 
-	close(rfd);
-	close(wfd);
-	
+		while ( (bytes = gzread(gzf, buf, BUF_SZ)) ) {
+			if (bytes < 0)
+				break;
+			_fwrite(buf, bytes, wfd);
+			putc('.', stderr);
+		}
+
+		gzclose(gzf);
+	} else {
+		while (fssize) {
+			bytes = (fssize > BUF_SZ) ? BUF_SZ : (int)fssize;
+			bytes = read(rfd, buf, bytes);
+			if (bytes < 0)
+				break;
+			_fwrite(buf, bytes, wfd);
+			putc('.', stderr);
+		}
+		
+		close(rfd);
+	}
+
+	putc('\n', stderr);
+
+	if (bytes < 0) {
+		perror("Failure loading ramdisk");
+		return 0;
+	}
+			
 	return 1;
 }
 		
