5884b8d26dbc761976d0dea0c046feb8df711b57
H. Peter Anvin <hpa@zytor.com>
2006-01-30 14:46:37 -0800

    Clean up fstype and move into a kinit subdirectory

--- usr/kinit/fstype/fstype.c
+++ usr/kinit/fstype/fstype.c
@@ -42,116 +42,90 @@
  */
 #include "reiserfs_fs.h"
 
-char *progname;
+#include "fstype.h"
 
 #define ARRAY_SIZE(x)	(sizeof(x) / sizeof(x[0]))
 
-#define BLOCK_SIZE_BITS 10
-#define BLOCK_SIZE	(1 << BLOCK_SIZE_BITS)
-#define BYTES_TO_BLOCKS(x) (((x) + (BLOCK_SIZE - 1)) >> BLOCK_SIZE_BITS)
+#define BLOCK_SIZE 1024
 
-static int fstype(const char *type, unsigned long size)
-{
-	fprintf(stdout, "FSTYPE=%s\nFSSIZE=%lu\n", type, size);
-	return 0;
-}
-
-static int readfile(int fd, off_t off, void *buf, size_t size)
-{
-	int ret;
-
-	if (lseek(fd, off, SEEK_SET) == -1) {
-		perror("lseek");
-		return 1;
-	}
-
-	ret = read(fd, buf, size);
-	if (ret == -1) {
-		perror("read");
-		return 1;
-	}
-	if (ret != BLOCK_SIZE) {
-		fprintf(stderr, "%s: short read\n", progname);
-		return 1;
-	}
-	return 0;
-}
-
-static int gzip_image(const unsigned char *buf, unsigned long *blocks)
+static int gzip_image(const unsigned char *buf, unsigned long long *bytes)
 {
 	if (buf[0] == 037 && (buf[1] == 0213 || buf[1] == 0236)) {
-		*blocks = 0;
+		*bytes = 0;	/* Can only be determined by processing the whole file */
 		return 1;
 	}
 	return 0;
 }
 
-static int cramfs_image(const unsigned char *buf, unsigned long *blocks)
+static int cramfs_image(const unsigned char *buf, unsigned long long *bytes)
 {
 	const struct cramfs_super *sb =
 		(const struct cramfs_super *)buf;
 
 	if (sb->magic == CRAMFS_MAGIC) {
 		if (sb->flags & CRAMFS_FLAG_FSID_VERSION_2)
-			*blocks = sb->fsid.blocks;
+			*bytes = (unsigned long long)sb->fsid.blocks << 10;
 		else
-			*blocks = 0;
+			*bytes = 0;
 		return 1;
 	}
 	return 0;
 }
 
-static int romfs_image(const unsigned char *buf, unsigned long *blocks)
+static int romfs_image(const unsigned char *buf, unsigned long long *bytes)
 {
         const struct romfs_super_block *sb =
 		(const struct romfs_super_block *)buf;
 
 	if (sb->word0 == ROMSB_WORD0 && sb->word1 == ROMSB_WORD1) {
-		*blocks = (unsigned long)BYTES_TO_BLOCKS(__be32_to_cpu(sb->size));
+		*bytes = __be32_to_cpu(sb->size);
 		return 1;
 	}
 	return 0;
 }
 
-static int minix_image(const unsigned char *buf, unsigned long *blocks)
+static int minix_image(const unsigned char *buf, unsigned long long *bytes)
 {
 	const struct minix_super_block *sb =
 		(const struct minix_super_block *)buf;
 
 	if (sb->s_magic == MINIX_SUPER_MAGIC ||
 	    sb->s_magic == MINIX_SUPER_MAGIC2) {
-		*blocks = sb->s_nzones << sb->s_log_zone_size;
+		*bytes = (unsigned long long)sb->s_nzones 
+			<< (sb->s_log_zone_size+10);
 		return 1;
 	}
 	return 0;
 }
 
-static int ext3_image(const unsigned char *buf, unsigned long *blocks)
+static int ext3_image(const unsigned char *buf, unsigned long long *bytes)
 {
 	const struct ext3_super_block *sb =
 		(const struct ext3_super_block *)buf;
 
 	if (sb->s_magic == __cpu_to_le16(EXT2_SUPER_MAGIC) &&
 	    sb->s_feature_compat & __cpu_to_le32(EXT3_FEATURE_COMPAT_HAS_JOURNAL)) {
-		*blocks = __le32_to_cpu(sb->s_blocks_count);
+		*bytes = (unsigned long long)__le32_to_cpu(sb->s_blocks_count)
+			<< (10+sb->s_log_block_size);
 		return 1;
 	}
 	return 0;
 }
 
-static int ext2_image(const unsigned char *buf, unsigned long *blocks)
+static int ext2_image(const unsigned char *buf, unsigned long long *bytes)
 {
 	const struct ext2_super_block *sb =
 		(const struct ext2_super_block *)buf;
 
 	if (sb->s_magic == __cpu_to_le16(EXT2_SUPER_MAGIC)) {
-		*blocks = __le32_to_cpu(sb->s_blocks_count);
+		*bytes = (unsigned long long)__le32_to_cpu(sb->s_blocks_count)
+			<< (10+sb->s_log_block_size);
 		return 1;
 	}
 	return 0;
 }
 
-static int reiserfs_image(const unsigned char *buf, unsigned long *blocks)
+static int reiserfs_image(const unsigned char *buf, unsigned long long *bytes)
 {
 	const struct reiserfs_super_block *sb =
 		(const struct reiserfs_super_block *)buf;
@@ -162,34 +136,33 @@ static int reiserfs_image(const unsigned char *buf, unsigned long *blocks)
 		   sizeof(REISER2FS_SUPER_MAGIC_STRING) - 1) == 0 ||
 	    memcmp(REISERFS_MAGIC(sb), REISER2FS_JR_SUPER_MAGIC_STRING,
 		   sizeof(REISER2FS_JR_SUPER_MAGIC_STRING) - 1) == 0) {
-		*blocks = REISERFS_BLOCK_COUNT(sb) *
-			  (REISERFS_BLOCKSIZE(sb) / BLOCK_SIZE);
+		*bytes = (unsigned long long)REISERFS_BLOCK_COUNT(sb) *
+			REISERFS_BLOCKSIZE(sb);
 		return 1;
 	}
 	return 0;
 }
 
-static int xfs_image(const unsigned char *buf, unsigned long *blocks)
+static int xfs_image(const unsigned char *buf, unsigned long long *bytes)
 {
 	const struct xfs_sb *sb =
 		(const struct xfs_sb *)buf;
 
 	if (__be32_to_cpu(sb->sb_magicnum) == XFS_SB_MAGIC) {
-		*blocks = __be64_to_cpu(sb->sb_dblocks) *
-			  (__be32_to_cpu(sb->sb_blocksize) / BLOCK_SIZE);
+		*bytes = __be64_to_cpu(sb->sb_dblocks) *
+			__be32_to_cpu(sb->sb_blocksize);
 		return 1;
 	}
 	return 0;
 }
 
-static int jfs_image(const unsigned char *buf, unsigned long *blocks)
+static int jfs_image(const unsigned char *buf, unsigned long long *bytes)
 {
 	const struct jfs_superblock *sb =
 		(const struct jfs_superblock *)buf;
 
 	if (!memcmp(sb->s_magic, JFS_MAGIC, 4)) {
-		/* 512 is the VFS Block size */
-		*blocks = __le32_to_cpu(sb->s_size) * 512;;
+		*bytes = __le32_to_cpu(sb->s_size);
 		return 1;
 	}
 	return 0;
@@ -198,7 +171,7 @@ static int jfs_image(const unsigned char *buf, unsigned long *blocks)
 struct imagetype {
 	off_t		block;
 	const char	name[12];
-	int		(*identify)(const unsigned char *, unsigned long *);
+	int		(*identify)(const unsigned char *, unsigned long long *);
 };
 
 static struct imagetype images[] = {
@@ -214,29 +187,15 @@ static struct imagetype images[] = {
 	{ 32,	"jfs",		jfs_image	}
 };
 
-int main(int argc, char *argv[])
+int identify_fs(int fd, const char **fstype, unsigned long long *bytes)
 {
 	unsigned char buf[BLOCK_SIZE];
-	unsigned long size = 0;
 	off_t cur_block = (off_t)-1;
-	unsigned int i;
+	int i;
 	int ret;
-	int fd = 0;
 
-	progname = argv[0];
-
-	if (argc > 2) {
-		fprintf(stderr, "Usage: %s [file]\n", progname);
-		return 1;
-	}
-
-	if (argc > 1 && strcmp(argv[1], "-")) {
-		fd = open(argv[1], O_RDONLY);
-		if ( fd < 0 ) {
-			perror(argv[1]);
-			return 1;
-		}
-	}
+	*fstype = NULL;
+	*bytes = 0;
 
 	for (i = 0; i < ARRAY_SIZE(images); i++) {
 		if (cur_block != images[i].block) {
@@ -244,16 +203,16 @@ int main(int argc, char *argv[])
 			 * Read block.
 			 */
 			cur_block = images[i].block;
-			ret = readfile(fd, cur_block * BLOCK_SIZE, buf,
-				       BLOCK_SIZE);
-			if (ret != 0)
-				return ret;
+			ret = pread(fd, buf, BLOCK_SIZE, cur_block*BLOCK_SIZE);
+			if (ret != BLOCK_SIZE)
+				return -1; /* error */
 		}
 
-		if (images[i].identify(buf, &size))
-			return fstype(images[i].name, size);
+		if (images[i].identify(buf, bytes)) {
+			*fstype = images[i].name;
+			return 0;
+		}
 	}
 
-	fstype("unknown", 0);
-	return 0;
+	return 1;		/* Unknown filesystem */
 }
---
+++ usr/kinit/fstype/fstype.h
@@ -0,0 +1,20 @@
+/*
+ * fstype.c, by rmk
+ *
+ * Detect filesystem type (on stdin) and output strings for two
+ * environment variables:
+ *  FSTYPE - filesystem type
+ *  FSSIZE - filesystem size (if known)
+ *
+ * We currently detect (in order):
+ *  gzip, cramfs, romfs, xfs, minix, ext3, ext2, reiserfs, jfs
+ *
+ * MINIX, ext3 and Reiserfs bits are currently untested.
+ */
+
+#ifndef FSTYPE_H
+#define FSTYPE_H
+
+int identify_fs(int fd, const char **fstype, unsigned long long *bytes);
+
+#endif
