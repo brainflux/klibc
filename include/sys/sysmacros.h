63852759defbcb671ba0fa608f8de58aa87b1cdf
H. Peter Anvin <hpa@zytor.com>
2006-04-04 13:24:10 -0700

    [klibc] Parentesize macro arguments properly
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- include/sys/sysmacros.h
+++ include/sys/sysmacros.h
@@ -12,20 +12,20 @@
 # include <sys/types.h>
 #endif
 
-#define __major(__d) ((__d >> 8) & 0xfff)
+#define __major(__d) (((__d) >> 8) & 0xfff)
 static __inline__ int major(dev_t __d)
 {
 	return __major(__d);
 }
 
-#define __minor(__d) ((__d & 0xff) | ((__d >> 12) & 0xfff00))
+#define __minor(__d) (((__d) & 0xff)|(((__d) >> 12) & 0xfff00))
 static __inline__ int minor(dev_t __d)
 {
 	return __minor(__d);
 }
 
 #define __makedev(__ma, __mi) \
-	(((__ma & 0xfff) << 8) | (__mi & 0xff) | ((__mi & 0xfff00) << 12))
+	((((__ma) & 0xfff) << 8)|((__mi) & 0xff)|(((__mi) & 0xfff00) << 12))
 static __inline__ dev_t makedev(int __ma, int __mi)
 {
 	return __makedev(__ma, __mi);
