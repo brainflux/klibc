ccf5d04af219e88c25afaae5271ee2b87fa11d8b
H. Peter Anvin <hpa@zytor.com>
2006-04-05 22:27:04 -0700

    [klibc] x86: the memory version of bit scan is weird, don't use it.
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- klibc/libc_init.c
+++ klibc/libc_init.c
@@ -93,7 +93,7 @@ __noreturn __libc_init(uintptr_t *elfdata, void (*onexit)(void))
   /* unsigned int is 32 bits on all our architectures */
   page_shift = __builtin_clz(page_size) ^ 31;
 #elif defined(__i386__) || defined(__x86_64__)
-  asm("bsrl %1,%0" : "=r" (page_shift) : "rm" (page_size));
+  asm("bsrl %1,%0" : "=r" (page_shift) : "r" (page_size));
 #else
   while ( page_size > 1 ) {
     page_shift++;
