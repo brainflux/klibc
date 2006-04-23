4109e7a9ce72e7dea60a76a541d136935b6c5c73
H. Peter Anvin <hpa@zytor.com>
2006-04-23 15:47:15 -0700

    [klibc] Use rt_sig*() system calls on i386
    On i386, only part of the signal space is accessible through the legacy
    calls.  In order to use the rt_sig*() calls, however, we have to implement
    our own signal header file instead of relying on <asm/signal.h>.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- include/arch/i386/klibc/archconfig.h
+++ include/arch/i386/klibc/archconfig.h
@@ -9,6 +9,9 @@
 #ifndef _KLIBC_ARCHCONFIG_H
 #define _KLIBC_ARCHCONFIG_H
 
+/* On i386, only half the signals are accessible using the legacy calls. */
+#define _KLIBC_USE_RT_SIG 1
+
 /* The stock i386 kernel is fine, but a whole string of Fedora kernels
    had a broken default restorer.  It's easier to enable this here. */
 #define _KLIBC_NEEDS_SA_RESTORER 1
