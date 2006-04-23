cfdbaad2180edde3f76d94267b513d55b2a37e4a
H. Peter Anvin <hpa@zytor.com>
2006-04-23 16:19:21 -0700

    [klibc] Fix warnings in strsignal.c
    Missing cast and missing header file; fix.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- klibc/strsignal.c
+++ klibc/strsignal.c
@@ -4,13 +4,14 @@
 
 #include <string.h>
 #include <signal.h>
+#include <stdio.h>
 
 char *strsignal(int sig)
 {
 	static char buf[64];
 
 	if ((unsigned)sig < _NSIG && sys_siglist[sig])
-		return sys_siglist[sig];
+		return (char *)sys_siglist[sig];
 
 #ifdef SIGRTMIN
 	if (sig >= SIGRTMIN && sig <= SIGRTMAX) {
