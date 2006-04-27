24eb09c4b8df5b2c634c8d5e01ed2e4c1045da0b
H. Peter Anvin <hpa@zytor.com>
2006-04-26 17:11:53 -0700

    [klibc] Make bdevname() work for partitions, and add test functionality
    This patch corrects the behaviour of bdevname() for partitions, and
    it adds the ability to build devname.c standalone for testing.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- usr/kinit/devname.c
+++ usr/kinit/devname.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/sysmacros.h>
-#include "kinit.h"
 
 /*
  * devname.c
@@ -12,55 +12,101 @@
  * Print the name of a block device.
  */
 
+static int scansysdir(char *namebuf, char *sysdir, dev_t dev)
+{
+	char *dirtailptr = strchr(sysdir, '\0');
+	DIR *dir;
+	int done = 0;
+	struct dirent *de;
+	char *systail;
+	FILE *sysdev;
+	unsigned long ma, mi;
+	char *ep;
+	ssize_t rd;
+
+	if (!(dir = opendir(sysdir)))
+		return 0;
+
+	*dirtailptr++ = '/';
+
+	while (!done && (de = readdir(dir))) {
+		/* Assume if we see a dot-name in sysfs it's special */
+		if (de->d_name[0] == '.')
+			continue;
+
+		if (de->d_type != DT_UNKNOWN && de->d_type != DT_DIR)
+			continue;
+
+		if (strlen(de->d_name) > 255)
+			continue; /* Badness... */
+
+		strcpy(dirtailptr, de->d_name);
+		systail = strchr(sysdir, '\0');
+
+		strcpy(systail, "/dev");
+		if (!(sysdev = fopen(sysdir, "r")))
+			continue;
+
+		/* Abusing the namebuf as temporary storage here. */
+		rd = fread(namebuf, 1, 256, sysdev);
+		namebuf[rd] = '\0'; /* Just in case... */
+
+		fclose(sysdev);
+
+		ma = strtoul(namebuf, &ep, 10);
+		if (ma != major(dev) || *ep != ':')
+			continue;
+
+		mi = strtoul(ep+1, &ep, 10);
+		if (*ep != '\n')
+			continue;
+
+		if (mi == minor(dev)) {
+			/* Found it! */
+			strcpy(namebuf, de->d_name);
+			done = 1;
+		} else {
+			/* we have a major number match, scan for partitions */
+			*systail = '\0';
+			done = scansysdir(namebuf, sysdir, dev);
+		}
+	}
+
+	closedir(dir);
+	return done;
+}
+
 const char *bdevname(dev_t dev)
 {
 	static char buf[512];
-	char sysnode[512-64];	/* Must be smaller than buf */
-	DIR *sysblock;
-	FILE *sysdev;
-	struct dirent *de;
-	unsigned long ma, mi;
-	int rd;
-	char *ep;
-	char *name = "dev";	/* Prints e.g. dev(3,5) */
+	char sysdir[512];
+	char *p;
 
-	if ((sysblock = opendir("/sys/block"))) {
-		while ((de = readdir(sysblock))) {
-			/* Assume if we see a dot-name in sysfs it's special */
-			if (de->d_name[0] == '.')
-				continue;
+	strcpy(sysdir, "/sys/block");
 
-			/* openfd() is really the right thing to use here... */
+	if (!scansysdir(buf, sysdir, dev))
+		strcpy(buf, "dev"); /* prints e.g. dev(3,5) */
 
-			if (snprintf(sysnode, sizeof sysnode,
-				     "/sys/block/%s/dev", de->d_name)
-			    >= sizeof sysnode)
-				continue; /* Weird! */
+	p = strchr(buf, '\0');
+	snprintf(p, sizeof buf-(p-buf), "(%d,%d)", major(dev), minor(dev));
 
-			if (!(sysdev = fopen(sysnode, "r")))
-				continue;
-
-			rd = fread(sysnode, 1, sizeof sysnode-1, sysdev);
-			sysnode[rd] = '\0'; /* Just in case... */
-
-			fclose(sysdev);
-
-			ma = strtoul(sysnode, &ep, 10);
-			if (ma != major(dev) || *ep != ':')
-				continue;
-
-			mi = strtoul(ep+1, &ep, 10);
-			if (mi != minor(dev) || *ep != '\n')
-				continue;
-
-			/* Found it! */
-			name = de->d_name;
-			break;
-		}
-		closedir(sysblock);
-	}
-
-	snprintf(buf, sizeof buf, "%s(%d,%d)",
-		 name, major(dev), minor(dev));
 	return buf;
 }
+
+#ifdef TEST			/* Standalone test */
+
+int main(int argc, char *argv[])
+{
+	dev_t dev;
+	int i;
+
+	for (i = 1; i < argc; i++) {
+		dev = strtoul(argv[i], NULL, 0);
+
+		printf("0x%08x = %s\n", (unsigned int)dev, bdevname(dev));
+	}
+
+	return 0;
+}
+
+#endif /* TEST */
