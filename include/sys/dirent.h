e29477bc27cdcbbb2ac41b64492e70f4bc8ca449
H. Peter Anvin <hpa@zytor.com>
2006-04-26 17:21:30 -0700

    [klibc] <sys/dirent.h>: add DT_* constants
    We can't include the kernel version of this file because of namespace
    issues, so we need to also provide our own versions of the DT_*
    constants.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- include/sys/dirent.h
+++ include/sys/dirent.h
@@ -16,6 +16,17 @@ struct dirent {
   char			d_name[256];
 };
 
+/* File types to use for d_type */
+#define DT_UNKNOWN	 0
+#define DT_FIFO		 1
+#define DT_CHR		 2
+#define DT_DIR		 4
+#define DT_BLK		 6
+#define DT_REG		 8
+#define DT_LNK		10
+#define DT_SOCK		12
+#define DT_WHT		14
+
 __extern int getdents(unsigned int, struct dirent *, unsigned int);
 
 #endif /* _SYS_DIRENT_H */
