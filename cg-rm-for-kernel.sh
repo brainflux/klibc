664eb8e962104fdac639690b4fec31ba31f14be8
H. Peter Anvin <hpa@zytor.com>
2006-05-08 11:07:34 -0700

    [klibc] Add a script to prune the klibc repository for kernel merge
    This script removes entries we do not want to carry over into the
    kernel tree.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

---
+++ cg-rm-for-kernel.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+if [ -z "$RM" ]; then
+  export RM=cg-rm
+fi
+
+nuke () {
+    find "$@" -print | rev | xargs -rt $RM
+}
+
+nuke Kbuild Makefile defconfig klibc.spec.in *.sh
+nuke contrib klcc
+
+# These files are either not needed or provided from the
+# kernel tree
+nuke scripts/Kbuild.include scripts/Kbuild.install
+nuke scripts/Makefile.*
+nuke scripts/basic
