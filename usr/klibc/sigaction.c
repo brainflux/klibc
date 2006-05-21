f77af169d800f139459f995a3f9c17fec22558c7
H. Peter Anvin <hpa@zytor.com>
2006-05-21 16:28:43 -0700

    [klibc] sparc: sparc has a messed up rt_sigaction() system call
    SPARC has a nonstandard rt_sigaction() system call, and does appear
    to not even have the SA_RESTORER flag.  Simply drop it for now.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- usr/klibc/sigaction.c
+++ usr/klibc/sigaction.c
@@ -8,8 +8,13 @@
 
 __extern void __sigreturn(void);
 __extern int __sigaction(int, const struct sigaction *, struct sigaction *);
+#ifdef __sparc__
+__extern int __rt_sigaction(int, const struct sigaction *, struct sigaction *,
+			    void *, size_t);
+#else
 __extern int __rt_sigaction(int, const struct sigaction *, struct sigaction *,
 			    size_t);
+#endif
 
 int sigaction(int sig, const struct sigaction *act, struct sigaction *oact)
 {
@@ -22,14 +27,19 @@ int sigaction(int sig, const struct sigaction *act, struct sigaction *oact)
 		sa = *act;
 		act = &sa;
 
-		/* The kernel can't be trusted to have a valid default restorer */
+		/* The kernel can't be trusted to have a valid default
+		   restorer */
 		sa.sa_flags |= SA_RESTORER;
 		sa.sa_restorer = &__sigreturn;
 	}
 #endif
 
 #if _KLIBC_USE_RT_SIG
+# ifdef __sparc__
+	rv = __rt_sigaction(sig, act, oact, NULL, sizeof(sigset_t));
+# else
 	rv = __rt_sigaction(sig, act, oact, sizeof(sigset_t));
+# endif
 #else
 	rv = __sigaction(sig, act, oact);
 #endif
