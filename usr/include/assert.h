0ba34a10b095d25601dacd540420a97b71278a28
H. Peter Anvin <hpa@zytor.com>
2006-06-25 15:03:28 -0700

    [klibc] __assert_fail() is a __noreturn function
    __assert_fail() never returns, and telling that to the compiler
    can sometimes generate better code.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- usr/include/assert.h
+++ usr/include/assert.h
@@ -5,13 +5,15 @@
 #ifndef _ASSERT_H
 #define _ASSERT_H
 
+#include <klibc/compiler.h>
+
 #ifdef NDEBUG
 
 #define assert(x) ((void)(x))
 
 #else
 
-extern void __assert_fail(const char *, const char *, unsigned int);
+extern __noreturn __assert_fail(const char *, const char *, unsigned int);
 
 #define assert(x) ((x) ? (void)0 : __assert_fail(#x, __FILE__, __LINE__))
 
--- usr/klibc/assert.c
+++ usr/klibc/assert.c
@@ -5,8 +5,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <klibc/compiler.h>
 
-void __assert_fail(const char *expr, const char *file, unsigned int line)
+__noreturn __assert_fail(const char *expr, const char *file, unsigned int line)
 {
 	printf("Assertion %s failed, file %s, line %u\n", expr, file, line);
 	abort();
