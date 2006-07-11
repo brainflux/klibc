0d844078c54afeec4af51ed80e11726f4fafc40e
H. Peter Anvin <hpa@zytor.com>
2006-07-11 12:24:33 -0700

    [klibc] mknod: add support for making fifos; don't use perror() w/o error
    Add support for making fifos; don't call perror() if there haven't been
    any errno-setting error.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- usr/utils/mknod.c
+++ usr/utils/mknod.c
@@ -6,50 +6,62 @@ char *progname;
 
 static __noreturn usage(void)
 {
-	fprintf(stderr, "Usage: %s name {b|c} major minor\n", progname);
+	fprintf(stderr, "Usage: %s name {b|c|f} major minor\n", progname);
 	exit(1);
 }
 
-int main(int argc, char *argv[], char *envp[])
+int main(int argc, char *argv[])
 {
-	char *name = NULL, *endp;
+	char *name, *type, typec, *endp;
 	unsigned int major, minor;
 	mode_t mode;
 	dev_t dev;
 
-	progname = argv[0];
-	if (argc != 5)
+	progname = *argv++;
+
+	name = *argv++;
+	if (!name)
 		usage();
 
-	name = argv[1];
-	if (!name) {
-		perror("device");
+	type = *argv++;
+	if (!type || !type[0] || type[1])
+		usage();
+	typec = type[0];
+
+	mode = 0;
+	switch (type[0]) {
+	case 'c':
+		mode = S_IFCHR;
+		break;
+	case 'b':
+		mode = S_IFBLK;
+		break;
+	case 'f':
+		mode = S_IFIFO;
+		break;
+	default:
 		usage();
 	}
 
-	mode = 0666;
-	if (argv[2][0] == 'c')
-		mode |= S_IFCHR;
-	else if (argv[2][0] == 'b')
-		mode |= S_IFBLK;
-	else {
-		perror("block or char devices.");
-		usage();
+	if (mode == S_IFIFO) {
+		dev = 0;
+	} else {
+		if (!argv[0] || !argv[1])
+			usage();
+			
+		major = strtol(*argv++, &endp, 0);
+		if (*endp != '\0')
+			usage();
+		minor = strtol(*argv++, &endp, 0);
+		if (*endp != '\0')
+			usage();
+		dev = makedev(major, minor);
 	}
 
-	major = strtol(argv[3], &endp, 0);
-	if (*endp != '\0') {
-		perror("major.");
+	if (*argv)
 		usage();
-	}
-	minor = strtol(argv[4], &endp, 0);
-	if (*endp != '\0') {
-		perror("minor.");
-		usage();
-	}
-	dev = makedev(major, minor);
 
-	if (mknod(name, mode, dev) == -1) {
+	if (mknod(name, mode|0666, dev) == -1) {
 		perror("mknod");
 		exit(1);
 	}
