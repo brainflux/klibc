e759e5bf85b4c6f895a31001a07a216699b07ccc
H. Peter Anvin <hpa@zytor.com>
2006-09-12 13:59:26 -0700

    [klibc] Remove SA_RESTORER/sigreturn hack for i386
    Remove the SA_RESTORER/sigreturn hack for i386.  On i386, the format
    of the sigreturn stub varies depeding on if SA_SIGINFO is set or not;
    the stock kernels were always OK (only some Fedora kernels were broken),
    and the Fedora kernels that were broken can now be considered historic.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- usr/include/arch/i386/klibc/archconfig.h
+++ usr/include/arch/i386/klibc/archconfig.h
@@ -12,8 +12,4 @@
 /* On i386, only half the signals are accessible using the legacy calls. */
 #define _KLIBC_USE_RT_SIG 1
 
-/* The stock i386 kernel is fine, but a whole string of Fedora kernels
-   had a broken default restorer.  It's easier to enable this here. */
-#define _KLIBC_NEEDS_SA_RESTORER 1
-
 #endif				/* _KLIBC_ARCHCONFIG_H */
