95543286c5d1587aac92c1cb7d3a51f9dd1c1696
H. Peter Anvin <hpa@zytor.com>
2008-07-07 16:05:17 -0700

    [klibc] memmove: when we do std, we need to also cld
    After doing std, we absolutely need to cld afterwards (the compiler
    has the right to assume DF=0, and we do elsewhere.)
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- usr/klibc/memmove.c
+++ usr/klibc/memmove.c
@@ -10,12 +10,12 @@ void *memmove(void *dst, const void *src, size_t n)
 	char *q = dst;
 #if defined(__i386__) || defined(__x86_64__)
 	if (q < p) {
-		asm volatile("cld ; rep ; movsb"
+		asm volatile("cld; rep; movsb"
 			     : "+c" (n), "+S"(p), "+D"(q));
 	} else {
 		p += (n - 1);
 		q += (n - 1);
-		asm volatile("std ; rep ; movsb"
+		asm volatile("std; rep; movsb; cld"
 			     : "+c" (n), "+S"(p), "+D"(q));
 	}
 #else
