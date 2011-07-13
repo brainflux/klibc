ee265272146d32be7bfc05536832a42e47faa468
maximilian attems <max@stro.at>
2011-07-13 18:02:52 +0200

    [klibc] header: Fix missing <sys/socket.h> include
    Found by autotools while configuring util-linux with
     ./configure  --without-ncurses  CC=klcc
    against klibc-1.5.23 version. It would complain:
    
    configure: WARNING: net/if.h: present but cannot be compiled
    configure: WARNING: net/if.h:     check for missing prerequisite headers?
    
    configure: WARNING: sys/un.h: present but cannot be compiled
    configure: WARNING: sys/un.h:     check for missing prerequisite headers?
    
    This where the only two and it both cases sa_family_t def seemed
    missing.
    
    Reviewed-by: H. Peter Anvin <hpa@zytor.com>
    Signed-off-by: maximilian attems <max@stro.at>

--- usr/include/net/if.h
+++ usr/include/net/if.h
@@ -1,6 +1,7 @@
 #ifndef _NET_IF_H
 #define _NET_IF_H
 
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <linux/if.h>
 
--- usr/include/sys/un.h
+++ usr/include/sys/un.h
@@ -5,6 +5,7 @@
 #ifndef _SYS_UN_H
 #define _SYS_UN_H
 
+#include <sys/socket.h>
 #include <linux/un.h>
 
 #endif				/* _SYS_UN_H */
