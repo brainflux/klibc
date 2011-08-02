0083b0b87387e9aa7363f1b3403b465855d4d1cd
maximilian attems <max@stro.at>
2011-08-02 23:15:06 +0200

    [klibc] include: [capability.h] kill names in arguments
    It breaks macros.
    
    Acked-by: Mike Waychison <mikew@google.com>
    Signed-off-by: maximilian attems <max@stro.at>

--- usr/include/sys/capability.h
+++ usr/include/sys/capability.h
@@ -4,7 +4,7 @@
 #include <klibc/extern.h>
 #include <linux/capability.h>
 
-__extern int capget(cap_user_header_t hdrp, cap_user_data_t datap);
-__extern int capset(cap_user_header_t hdrp, const cap_user_data_t datap);
+__extern int capget(cap_user_header_t, cap_user_data_t);
+__extern int capset(cap_user_header_t, const cap_user_data_t);
 
 #endif				/* _SYS_CAPABILITY_H */
